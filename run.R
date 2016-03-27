5ab2afd15a1c91289d437198f6cc43dc148abbe9
Barbara Sanchez-Neri <sanchezneri.barbara@gmail.com>
2016-03-27 19:32:07 -0400

    Included lines that download data to disk

--- run.R
+++ run.R
@@ -1,3 +1,7 @@
+URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(URL, "dataset.zip")
+unzip("dataset.zip")
+
 source("plot1.R")
 source("plot2.R")
 source("plot3.R")
